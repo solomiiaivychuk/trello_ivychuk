693e333ed739ee2cb44f4305b911a88dda3f1bc0
solomiiaivychuk <52928387+solomiiaivychuk@users.noreply.github.com>
2019-07-31 22:03:02 +0300

    Added_board_creation

---
+++ trello_web/src/test/java/com/telran/tests/CreateBoardtest.java
@@ -0,0 +1,68 @@
+package com.telran.tests;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CreateBoardtest extends TestBase {
+
+    @Test
+    public void boardCreationTest() throws InterruptedException {
+        clickOnLoginButton();
+        fillUserForm("tester28490@gmail.com", "Password234");
+        confirmLoginButton();
+        pause(5000);
+        clickOnPlusButtonOnHeader();
+        selectCreateBoardFromDropdown();
+        fillBoardCreationForm("MyBoard");
+        confirmBoardCreation();
+        clickOnHomeButtonOnHeader();
+        pause(2000);
+        addListToBoard();
+        inputListTitle("MyFirstList");
+        confirmListCreation();
+        pause(2000);
+        addCard();
+        inputCardName("MyCard");
+        confirmCardCreation();
+        pause(5000);
+        clickOnHomeButtonOnHeader();
+        pause(10000);
+        //clickOnAccountButton();
+        //logOut();
+    }
+
+    private void selectCreateBoardFromDropdown() {
+        click(By.xpath("//*[@data-test-id='header-create-board-button']"));
+    }
+    private void clickOnPlusButtonOnHeader() {
+        click(By.cssSelector("[name=add]"));
+    }
+
+    private void fillBoardCreationForm(String title) {
+        driver.findElement(By.xpath("//input[@placeholder='Add board title']")).sendKeys(title);
+    }
+    private void confirmBoardCreation() {
+    click(By.xpath("//*[@data-test-id='header-create-board-submit-button']"));
+    }
+   private void clickOnHomeButtonOnHeader() {
+       click(By.xpath("//span[@name='house']"));
+   }
+   private void addListToBoard() {
+        click(By.xpath("//span[@class='placeholder']"));
+   }
+   private void inputListTitle(String listTitle) {
+        driver.findElement(By.xpath("//input[@placeholder='Enter list title...']")).sendKeys(listTitle);
+   }
+   private void confirmListCreation() {
+        click(By.xpath("//input[@class='primary mod-list-add-button js-save-edit']"));
+   }
+   private void addCard() {
+        click(By.xpath("//span[@class='js-add-a-card']"));
+   }
+   private void inputCardName(String cardName) {
+        driver.findElement(By.xpath("//textarea[@placeholder='Enter a title for this card…']")).sendKeys(cardName);
+   }
+   private void confirmCardCreation() {
+        click(By.xpath("//input[@class='primary confirm mod-compact js-add-card']"));
+   }
+}
--- trello_web/src/test/java/com/telran/tests/LogInTest.java
+++ trello_web/src/test/java/com/telran/tests/LogInTest.java
@@ -1,4 +1,21 @@
 package com.telran.tests;
 
-public class LogInTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class LogInTest extends TestBase {
+
+    @Test
+    public void loginTest() throws InterruptedException {
+        clickOnLoginButton();
+        fillUserForm("tester28490@gmail.com", "Password234");
+        clickOnLoginButton();
+        pause(20000);
+    }
 }
